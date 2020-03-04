c364f9f5aa23e81c59c0b97416529693d011a5bf
MariaMartori <maria.martorii@students.salle.url.edu>
2020-03-04 14:31:25 +0100

    #1 feat: Added message
    Added a message to the main to inform the user that the program is running.

--- src/Model/Main.java
+++ src/Model/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args){
         SwingUtilities.invokeLater(() -> {
             MainView v = new MainView();
-            System.out.println("hola mon");
+            System.out.println("hola mon!!!");
             MainController c = new MainController(v);
             v.registerControler(c);
             v.setVisible(true);
